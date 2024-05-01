8b2b16809f207963864d4f455d99ac1558bcee61
jgrk28 <jacobkaplan28@gmail.com>
2024-05-01 08:41:00 -0500

    Create api for storing and retrieving videos

--- server/video_detection/models.py
+++ server/video_detection/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    file_name = models.CharField(max_length=100)
+    video = models.FileField(upload_to='videos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
--- server/video_detection/views.py
+++ server/video_detection/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Video
+from .serializers import VideoSerializer
 
-# Create your views here.
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
