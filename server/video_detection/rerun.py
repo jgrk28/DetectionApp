376a06558c6de2f74962c74c2730a1741d4095f6
jgrk28 <jacobkaplan28@gmail.com>
2024-05-02 09:20:00 -0500

    Disconnect rerun before starting a new one each time
    There were some bugs coming from the rerun connection persisting.
    Also now ensure that it will always serve to the same ports.

--- server/video_detection/rerun.py
+++ server/video_detection/rerun.py
@@ -1,9 +1,12 @@
 import rerun as rr
 import cv2
 
+from decouple import config
+
 def display_video(video):
+    rr.disconnect()
     rr.init("object_detection_rerun")
-    rr.serve(open_browser=False)
+    rr.serve(open_browser=False, web_port=int(config('RR_WEBPORT')), ws_port=int(config('RR_WSPORT')))
 
     print(f"Displaying rerun for: {video.video.path}")
     video = cv2.VideoCapture(video.video.path)
