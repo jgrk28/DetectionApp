273e6a0d75d250d8cb08c6c8de1926472d53d910
jgrk28 <jacobkaplan28@gmail.com>
2024-05-03 10:47:00 -0500

    Clean up react app and add example test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// If this project were to continue to develop we would want some regression testing
+// Each component would have a test.js file and we would change the states of the
+// components to make sure they were rendering correctly
+test('renders upload page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Upload Video')).toBeInTheDocument();
 });
